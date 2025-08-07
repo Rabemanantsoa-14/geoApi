37ea4c2a6bdefda5ec8b5077895728f47fb04e35
Bobby <andria.rabemanantsoa@gmail.com>
2025-08-07 14:55:03 +0300

    Description des changements

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT ?? 3000);
+  // Utiliser le port de Render si défini, sinon 3000 en local
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+  console.log(`Application démarrée sur le port ${port}`);
 }
 
 bootstrap();
